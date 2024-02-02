5c65376305ca1662a75996705247cd2b31a81830
Sandra Müllner <s_mue_@outlook.com>
2024-02-02 21:58:29 +0100

    Worked on main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
 # Entry point for the game
+
+import grid
+
+# Player Setup
+player1 = input("Ready, Player? What's your name?\n")
+player2 = "Computer-Player"
+
+# Coin Flip who starts:
+from random import randrange
+coin = randrange(2)
+
+if coin == 0:
+    beginner = player1
+    print(beginner, "starts.")
+else:
+    beginner = player2
+    print(beginner, "starts.")
+
+# Create a grid by calling initialize_grid
+create_grid = grid.initialize_grid()
+
+
+print(grid.print_grid(create_grid))
