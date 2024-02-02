f7da73db21ae8d224a2d6141e6f2e0f19c68df69
Sandra Müllner <s_mue_@outlook.com>
2024-02-02 22:07:24 +0100

    Added coordinate variables to grid module

--- src/grid.py
+++ src/grid.py
@@ -1,3 +1,7 @@
+# Coordinates ## Original Sandra
+coordinates_x = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10}
+coordinates_y = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10}
+
 # Initialize Grid ## Original Alexandra, Adapt Sandra
 def initialize_grid(size=10):
     # Create an empty grid with headers
