56244715e520334856f5ece245e21a74ec7bbfc7
Sandra Müllner <s_mue_@outlook.com>
2024-02-03 11:49:24 +0100

    Updated coordinate dictionaries up to 20 spaces

--- src/grid.py
+++ src/grid.py
@@ -1,8 +1,20 @@
 # Coordinates ## Original Sandra
-coordinates_x = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10}
-coordinates_y = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10}
+coordinates_x = {
+    'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5,
+    'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
+    'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15,
+    'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20
+    }
+
+coordinates_y = {
+    '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+    '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+    '11': 11, '12': 12, '13': 13, '14': 14, '15': 15,
+    '16': 16, '17': 17, '18': 18, '19': 19, '20': 20
+    }
 
 # Initialize Grid ## Original Alexandra, Adapt Sandra
+
 def initialize_grid(size=10):
     # Create an empty grid with headers
     grid = []
@@ -32,3 +44,4 @@ def print_grid(grid):
             print(f"{cell:<2} ", end="")
         print()
     print()
+
