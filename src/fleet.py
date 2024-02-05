45318a3be86ffa5b1c0d521ca617d5ed489c9d96
Sandra Müllner <s_mue_@outlook.com>
2024-02-05 22:24:54 +0100

    Made changes to fleet

--- src/fleet.py
+++ src/fleet.py
@@ -65,24 +65,5 @@ class Fleet:
 
     #### TODO update ship instances or fleet instances, not sure what and how yet, when placing ships on grid with coordinates ####
 
-# Creating ship instances
-
-destroyer_1_pl = Destroyer("Destroyer 1")
-destroyer_2_pl = Destroyer("Destroyer 2")
-cruiser_1_pl = Cruiser("Cruiser 1")
-battleship_1_pl = Battleship("Battleship 1")
-aircraft_carrier_1_pl = AircraftCarrier("Aircraft Carrier 1")
-
-destroyer_1_pc = Destroyer("Destroyer 1")
-destroyer_2_pc = Destroyer("Destroyer 2")
-cruiser_1_pc = Cruiser("Cruiser 1")
-battleship_1_pc= Battleship("Battleship 1")
-aircraft_carrier_1_pc = AircraftCarrier("Aircraft Carrier 1")
-
-# Creating Fleet instances and adding the ship instances to it
-
-fleet_player = Fleet("Fleet Player", destroyer_1_pl, destroyer_2_pl, cruiser_1_pl, battleship_1_pl, aircraft_carrier_1_pl)
-fleet_computer = Fleet("Fleet Computer", destroyer_1_pc, destroyer_2_pc, cruiser_1_pc, battleship_1_pc, aircraft_carrier_1_pc)
-
-# print(f"The Player's Fleet: {fleet_player.ships}")
-# print(f"The Computer's Fleet: {fleet_computer.ships}")
+#print(f"The Player's Fleet: {fleet_player.ships}")
+#print(f"The Computer's Fleet: {fleet_computer.ships}")
