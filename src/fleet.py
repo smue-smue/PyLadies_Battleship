d2f169ab38d6c05455d028cf9d01d5c171d84ed3
Sandra Müllner <s_mue_@outlook.com>
2024-02-05 18:07:25 +0100

    Updated docstring and get_fleet_size

--- src/fleet.py
+++ src/fleet.py
@@ -10,7 +10,7 @@ class Fleet:
     Attributes:
         name (str): Name of the fleet.
         ships (dict): Dictionary to store ship instances, with ship names as keys and
-                      dictionaries containing 'size' and 'coordinates' as values.
+                      nested dictionaries containing 'size' and 'coordinates' as values.
 
     Methods:
         __init__(self, name, *ships): Constructor for Fleet class.
@@ -21,16 +21,47 @@ class Fleet:
     max_size = 16
 
     def __init__(self, name, *ships): # '*ships' stands for a variable number of ship instances
+        '''
+        Initialize a Fleet instance.
+        
+        Parameters:
+            name(str): Name of the fleet.
+            *ships (Ship): Variable number of ship instances to be added to the fleet.
+        '''
+
         self.name = name
         self.ships = {}
         for ship in ships:
             self.add_ship(ship)
 
     def add_ship(self, ship): # Attribute ship is an instance of an Ship object
+        '''
+        Adds a ship instance to the fleet.
+
+        Parameters:
+            ship (Ship): Instance of Ship object to be added to the fleet. 
+                         The ship must have 'name' and 'size' attributes. 
+        
+        Updates:
+            self.ships: Updates the ships dictionary with the new ship's name as key
+                        and a dictionary containing 'size' and 'coordinates'
+                        (initially empty) as value.
+        
+        Return:
+            None        
+        '''
+        
         self.ships[ship.name] = {'size': ship.size, 'coordinates': []} # Adds instance of Ship object to the fleet where it is called on
 
     def get_fleet_size(self):
-        pass
+        '''
+        Return the current size of the fleet.
+        
+        Returns:
+            int: Number of ships currently in the fleet.
+        '''
+
+        return len(self.ships)
 
     #### TODO update ship instances or fleet instances, not sure what and how yet, when placing ships on grid with coordinates ####
 
