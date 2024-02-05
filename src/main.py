512ebb1eb7f06e326de69b12dd104eefbd452f9d
Sandra Müllner <s_mue_@outlook.com>
2024-02-05 19:02:48 +0100

    Moved stuff back to main, very messy

--- src/main.py
+++ src/main.py
@@ -1,59 +1,26 @@
 # Entry point for the game
-
-import grid
 from random import randrange
 
-#### TEMPORARY PLACE ####
-def ship_destroyer(coordinate_1, coordinate_2):
-    return(coordinate_1, coordinate_2)
+import grid
+import fleet
+from player import Player
 
+# Set up Player
+player_name = Player.prompt_for_player_name() # Calls the static method on the class
+player = Player(player_name) # Creates a new Player instance with the provided name
 
-def player_ships():
-    '''This function asks the player where to place their ships.'''
-
-    fleet = {
-            2: {"1st destroyer": [], "2nd destroyer": []},
-            3: {"Cruiser": []},
-            4: {"Battleship": []},
-            5: {"Aircraft Carrier": []}
-            }
- 
-    while True:
-        for shipsize in fleet.keys():
-                for shiptype in fleet[shipsize].keys():
-                    for coordinate in range(shipsize):
-                        coordinate = input(f"Please enter a coordinate of your {shiptype} (total size: {shipsize} squares). ")
-                        fleet[shipsize][shiptype].append(coordinate)
-                        # Hier sicherstellen, dass c1 bei c2 etc. anschließt
-                # print grid for overview after each ship
-        print(fleet)
-        return fleet
-
-
-# def position_ships(create_grid, coordinates_x, coordinates_y, choice): # needs to be redone!
-#     '''This function places the players ships on the grid.'''
-#     coord_1 = coordinates_x[choice[0]]
-#     coord_2 = coordinates_y[choice[1]]
-#     create_grid[coord_2][coord_1] = "X"
-#     updated_grid = create_grid
-#     return updated_grid
-
-#################################
-
-# Player Setup
-player1 = input("Ready, Player? What's your name?\n")
-player2 = "Computer-Player"
+# Set up Computer?
 
 # Coin Flip who starts:
 
 coin = randrange(2)
 
 if coin == 0:
-    beginner = player1
+    beginner = player
     print(beginner, "starts.")
 else:
-    beginner = player2
-    print(beginner, "starts.")
+    #beginner = computer
+    #print(computer, "starts.")
 
 # Create a grid by calling initialize_grid
 create_grid = grid.initialize_grid()
@@ -61,7 +28,7 @@ grid.print_grid(create_grid)
 
 # Game logic
 
-choice = player_ships()
+#choice = player_ships() # old
 #updated_grid = position_ships(create_grid, grid.coordinates_x, grid.coordinates_y, choice)
 
 #grid.print_grid(updated_grid)
