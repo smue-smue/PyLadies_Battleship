ae1b38a0a2871e9b8ed747534bc6f1373a60e870
Sandra Müllner <s_mue_@outlook.com>
2024-02-05 18:19:18 +0100

    Added docstring to class Ship

--- src/ship.py
+++ src/ship.py
@@ -1,22 +1,65 @@
 class Ship:
+    '''
+    A Class to represent a general ship.
+
+    Parameters:
+        name (str): Name of the ship.
+        type (str): Type of the ship (e.g., Destroyer)
+        size (int): Size of the ship, representing how many spaces
+                    of the grid it takes up.
+
+    '''
     def __init__(self, name, type, size):
-        self.name = name
-        self.type = type
-        self.size = size
+        self.name = name # Attribute: Name of the ship
+        self.type = type # Attribute: Type of the ship
+        self.size = size # Attribute: Size of the ship
 
 class Destroyer(Ship):
+    '''
+    A Subclass of Ship representing a destroyer, a smaller warship.
+
+    Parameters:
+        name (str): Name of the destroyer.
+
+    Inherits from Ship with a fixed type "Destroyer" and size 2.
+
+    ''' 
     def __init__(self, name):
         super().__init__(name, "Destroyer", 2)
 
 class Cruiser(Ship):
+    '''
+    A subclass of Ship representing a cruiser, a medium-sized warship.
+
+    Parameters:
+        name (str): The name of the cruiser.
+
+    Inherits from Ship with a fixed type "Cruiser" and size 3.
+    '''
     def __init__(self, name):
         super().__init__(name, "Cruiser", 3)
 
 class Battleship(Ship):
+    '''
+    A subclass of Ship representing a battleship, heavily armed and armored.
+
+    Parameters:
+        name (str): The name of the battleship.
+
+    Inherits from Ship with a fixed type "Battleship" and size 4.
+    '''
     def __init__(self, name):
         super().__init__(name, "Battleship", 4)
 
 class AircraftCarrier(Ship):
+    '''
+    A subclass of Ship representing an aircraft carrier, a warship that serves as a airbase.
+
+    Parameters:
+        name (str): The name of the aircraft carrier.
+
+    Inherits from Ship with a fixed type "Aircraft Carrier" and size 5.
+    '''
     def __init__(self, name):
         super().__init__(name, "Aircraft Carrier", 5)
 
