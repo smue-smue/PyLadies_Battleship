4c65b429bd72e07e91f1cfaaec6e65abce70a1b6
Sandra Müllner <s_mue_@outlook.com>
2024-02-09 13:32:27 +0100

    Updated documentation

--- src/fleet.py
+++ src/fleet.py
@@ -5,17 +5,20 @@ class Fleet:
     A class to represent a fleet of ships.
 
     Class Attributes:
-        max_size (int): Class attribute to define the maximum size of the fleet. Default is 16.
+        max_size    (int):  Class attribute to define the maximum size of the fleet. Default is 16.
 
     Attributes:
-        name (str): Name of the fleet.
-        ships (dict): Dictionary to store ship instances, with ship names as keys and
-                      nested dictionaries containing 'size' and 'coordinates' as values.
+        name        (str):      Name of the fleet.
+        ships       (dict):     Dictionary to store ship instances, with ship names as keys and
+                                nested dictionaries containing 'size' and 'coordinates' as values.
 
     Methods:
         __init__(self, name, *ships): Constructor for Fleet class.
         add_ship(self, ship): Adds a ship instance to the fleet.
         get_fleet_size(self): Returns the current size of the fleet.
+
+    Example:
+        >>> fleet_player = Fleet("Fleet Player", destroyer_1_pl, destroyer_2_pl, cruiser_1_pl, battleship_1_pl, aircraft_carrier_1_pl)
     '''
 
     max_size = 16
@@ -25,9 +28,11 @@ class Fleet:
         Initialize a Fleet instance.
         
         Parameters:
-            name(str): Name of the fleet.
-            *ships (Ship): Variable number of ship instances to be added to the fleet.
+            name    (str):      Name of the fleet.
+            *ships  (Ship):     Variable number of ship instances to be added to the fleet.
         '''
+
+        # TODO: Raise ValueError: If the number of ships provided exceeds the max_size.
 
         self.name = name
         self.ships = {}
@@ -36,11 +41,11 @@ class Fleet:
 
     def add_ship(self, ship): # Attribute ship is an instance of an Ship object
         '''
-        Adds a ship instance to the fleet.
+        Adds a ship instance to the fleet. If a ship with the same name already exists, its details are updated.
 
         Parameters:
-            ship (Ship): Instance of Ship object to be added to the fleet. 
-                         The ship must have 'name' and 'size' attributes. 
+            ship    (Ship):     Instance of Ship object to be added to the fleet. 
+                                The ship must have 'name' and 'size' attributes. 
         
         Updates:
             self.ships: Updates the ships dictionary with the new ship's name as key
@@ -50,12 +55,14 @@ class Fleet:
         Return:
             None        
         '''
+
+        # TODO: Raises: ValueError: If adding the ship would exceed the fleet's max_size.
         
         self.ships[ship.name] = {'size': ship.size, 'coordinates': []} # Adds instance of Ship object to the fleet where it is called on
 
     def get_fleet_size(self):
         '''
-        Return the current size of the fleet.
+        Return the current size of the fleet (the number of ships it contains.)
         
         Returns:
             int: Number of ships currently in the fleet.
