b108df9c521434da339f0ba617abf4abf60ba26a
Alexandra Falk-Schmidtberger <nebulaleaf@gmail.com>
2024-02-11 23:52:57 +0100

    Docstrings and ToDo

--- src/computer.py
+++ src/computer.py
@@ -1,15 +1,29 @@
+# Description: This file contains the computer's logic for placing ships on the grid.
+
 import random
 from grid import Grid
 
 def random_coordinate(grid_size):
+    '''
+    Returns a random coordinate within the grid bounds.
+    '''
+    
     column_label = random.choice('ABCDEFGHIJ'[0:grid_size]) 
     row_number = str(random.randint(1, grid_size))
     return column_label + row_number
 
 def random_direction():
+    '''
+    Returns a random direction: 'H' for horizontal or 'V' for vertical.
+    '''
+    
     return random.choice(['H', 'V'])
 
 def is_valid_placement(grid, start_coordinate, direction, size):
+    '''
+    Checks if the ship placement is valid.
+    '''
+
     column_label, row_number = start_coordinate[0], int(start_coordinate[1:])
     column_index = grid.coordinates_x[column_label] - 1  # Adjust for 0-based index
     row_index = row_number - 1  # Adjust for 0-based index
@@ -30,6 +44,10 @@ def is_valid_placement(grid, start_coordinate, direction, size):
     return True  # Valid placement
 
 def place_ships_randomly(fleet, grid):
+    '''
+    Places the ships in the fleet randomly on the grid.
+    '''
+    
     for shipname, shipdetails in fleet.items():
         placed = False
         while not placed:
--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ battleship_1_pc= Battleship("Battleship 1")
 aircraft_carrier_1_pc = AircraftCarrier("Aircraft Carrier 1")
 
 # Creating Fleet instances and adding the ship instances to it
-#TODO : Add a Timer, that Player has like 2 Minutes for placing the ships
+#TODO: Add a Timer, that Player has like 2 Minutes for placing the ships
 
 fleet_player = Fleet("Fleet Player", destroyer_1_pl, destroyer_2_pl, cruiser_1_pl, battleship_1_pl, aircraft_carrier_1_pl)
 print(type(fleet_player))
@@ -57,5 +57,5 @@ board_computer.print_grid() # TODO: löschen
 # Game logic
 
 player.player_placing_ships(fleet_player.ships, board_player) # Parameter: dict ships of the instance fleet_player from the class Fleet
-place_ships_randomly(fleet_computer.ships, board_computer)
+place_ships_randomly(fleet_computer.ships, board_computer)  # Parameter: dict ships of the instance fleet_computer from the class Fleet
 board_computer.print_grid() # TODO: löschen, nur zum nachschauen, ob das Grid befüllt wird
