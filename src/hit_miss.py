649d6d742f17d04cc3990c8b5342d3f03a3f6b6d
Sandra Müllner <s_mue_@outlook.com>
2024-02-17 21:51:27 +0100

    Made get_hit_coordinate

--- src/hit_miss.py
+++ src/hit_miss.py
@@ -8,3 +8,10 @@ def check_hit_or_miss(coordinate, grid):
         return 'hit'
     else:
         return 'miss'
+    
+def get_hit_coordinate(coordinate, grid):
+    column_index, row_index = grid._convert_coordinate_to_indices(coordinate)
+    cell = grid.grid[row_index][column_index]
+    if cell == 'S':
+        return coordinate
+    return None # Explicitly return None if there's no hit
