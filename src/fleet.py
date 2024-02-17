7f6627fd222ea8a12981fcf6a55143e3c2f24240
Sandra Müllner <s_mue_@outlook.com>
2024-02-17 21:36:59 +0100

    Added 'hits' key to self.ships

--- src/fleet.py
+++ src/fleet.py
@@ -58,7 +58,7 @@ class Fleet:
 
         # TODO: Raises: ValueError: If adding the ship would exceed the fleet's max_size.
         
-        self.ships[ship.name] = {'size': ship.size, 'coordinates': []} # Adds instance of Ship object to the fleet where it is called on
+        self.ships[ship.name] = {'size': ship.size, 'coordinates': [], 'hits': []} # Adds instance of Ship object to the fleet where it is called on
 
     def get_fleet_size(self):
         '''
@@ -69,3 +69,6 @@ class Fleet:
         '''
 
         return len(self.ships)
+
+
+
